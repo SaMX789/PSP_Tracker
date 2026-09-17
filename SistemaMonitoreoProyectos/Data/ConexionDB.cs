f17cda6b5880e53e8822d43ff0890cf140118be4
SaMX789 <raiozv789@gmail.com>
2026-09-17 09:59:19 -0600

    Version beta 1.0.0

--- SistemaMonitoreoProyectos/Data/ConexionDB.cs
+++ SistemaMonitoreoProyectos/Data/ConexionDB.cs
@@ -1,19 +1,17 @@
 ﻿using Microsoft.Data.Sqlite;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace SistemaMonitoreoProyectos.Data
 {
     public static class ConexionDB
     {
-        private const string CadenaConexion = "Data Source=Data/psp_tracker.db";
+        // Se ejecuta automáticamente al primer uso de ConexionDB y almacena la ruta de AppData
+        private static readonly string RutaBaseDatos = InicializarBaseDatos();
 
         public static SqliteConnection ObtenerConexion()
         {
-            var conexion = new SqliteConnection(CadenaConexion);
+            var conexion = new SqliteConnection($"Data Source={RutaBaseDatos}");
             conexion.Open();
 
             using var comando = conexion.CreateCommand();
@@ -21,6 +19,30 @@ namespace SistemaMonitoreoProyectos.Data
             comando.ExecuteNonQuery();
 
             return conexion;
+        }
+
+        private static string InicializarBaseDatos()
+        {
+            string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string carpetaDestino = Path.Combine(localAppData, "PSPTracker", "Data");
+            string rutaDestino = Path.Combine(carpetaDestino, "psp_tracker.db");
+
+            if (!File.Exists(rutaDestino))
+            {
+                Directory.CreateDirectory(carpetaDestino);
+                string rutaSemilla = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "psp_tracker.db");
+
+                if (File.Exists(rutaSemilla))
+                {
+                    File.Copy(rutaSemilla, rutaDestino, overwrite: true);
+                }
+                else
+                {
+                    throw new FileNotFoundException("No se encontró la plantilla base psp_tracker.db en la carpeta del ejecutable.");
+                }
+            }
+
+            return rutaDestino;
         }
     }
 }
